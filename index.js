c94c31ea958ae2ae3d1a47c2963094a97a656689
Austin Turnage <austin@datarats.com>
2015-03-31 21:36:55 -0400

    added static html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express=require('express');
 var app=express();
 
 app.get('/', function(req, res){
-  res.send('Coming Soon');
+  res.sendFile('index.html');
 })
 
 app.listen(process.env.PORT || 3000 )
