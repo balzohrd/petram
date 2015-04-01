791e6b958f3ae8eb226019ec1dc96305650cd9a4
Austin Turnage <austin@datarats.com>
2015-03-31 21:50:00 -0400

    duh

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var express=require('express');
-var app=express();
+var express=require('express'),
+app=express(),
+path=require('path');
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.listen(process.env.PORT || 3000 )
